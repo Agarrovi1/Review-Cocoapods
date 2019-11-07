41848246f6d7b71b53f996368b7b8500ec7d43c1
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-11-07 10:42:30 -0500

    initial commit

---
+++ Review-Cocoapods/GetLibrary.swift
@@ -0,0 +1,26 @@
+//
+//  GetLibrary.swift
+//  Review-Cocoapods
+//
+//  Created by Angela Garrovillas on 11/7/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+
+struct GetLocation {
+    static func getData(name: String, type: String) -> Data {
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            fatalError("\(name) \(type) file not found")
+        }
+        let internalURL = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: internalURL)
+            return data
+        } catch {
+            fatalError("An error has occured: \(error)")
+        }
+    }
+}
---
+++ Review-Cocoapods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Review-Cocoapods
+//
+//  Created by Angela Garrovillas on 11/7/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
